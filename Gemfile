d4d107d9957674801cbf6bb00c15bb6f4ba1d334
sawada <sawadasuiren@gmail.com>
2015-09-23 17:20:33 +0900

    unicornで起動するように修正

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'mysql2', '~> 0.3.20'
 gem 'activerecord'
+gem 'unicorn'
---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+listen 3000
+timeout 180
+woker_processes 4
+
+pid "/var/run/Home.pid"
+stderr_path "/var/log/Home.log"
+stdout_path "/var/log/Home.err"
